6b24f5ec00c488b8d8d518eded6f89f32a5b3666
rarzate <rarzate@globalsoftm.com>
2023-07-19 11:07:46 -0600

    Initial Commit

---
+++ MultiPartTest/MultiPartTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPartTestApp.swift
+//  MultiPartTest
+//
+//  Created by Rafael Arzate on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiPartTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
